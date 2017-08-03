d5ecf9cd869267d635dec21c54d6310cb5854503
Krishnaraj Menon <menon.krishnaraj@gmail.com>
2017-08-03 22:55:32 +0530

    Adding SQL deadlock graph

---
+++ SQL/SQL Debug - Deadlock Graph.sql
@@ -0,0 +1,14 @@
+SELECT XEvent.query('(event/data/value/deadlock)[1]') AS DeadlockGraph
+FROM ( SELECT XEvent.query('.') AS XEvent
+	FROM (SELECT CAST(target_data as XML) AS TargetData
+	FROM sys.dm_xe_session_targets st
+	JOIN sys.dm_xe_sessions s
+	ON s.address = st.event_session_address
+	WHERE s.name = 'system_health'
+		AND st.target_name = 'ring_buffer'
+	) AS Data
+	CROSS APPLY
+	TargetData.nodes
+		('RingBufferTarget/event[@name="xml_deadlock_report"]')
+	AS XEventData (XEvent)
+) AS src;
