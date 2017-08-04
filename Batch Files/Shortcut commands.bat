e069ceedd413d44c7e8fa03d4c11bcdcdda3fef2
Krishnaraj Menon <menon.krishnaraj@gmail.com>
2017-08-04 15:07:01 +0530

    Update Shortcut commands.bat

--- Batch Files/Shortcut commands.bat
+++ Batch Files/Shortcut commands.bat
@@ -2,7 +2,7 @@ rem Find out all ports being used in your system
 rem netstat -ab
 
 rem Copy any word to the clipboard
-rem echo krishnaraj.menon|clip
+rem echo {any word}|clip
 
 rem Start Internet explorer with a URL
 rem start iexplore http://www.google.com
