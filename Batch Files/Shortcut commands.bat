a9325c15f044a08ec20b6cc14d47a47f82f53e9d
Krishnaraj Menon <menon.krishnaraj@gmail.com>
2017-08-04 08:31:49 +0530

    Updated Commands

--- Batch Files/Shortcut commands.bat
+++ Batch Files/Shortcut commands.bat
@@ -4,8 +4,11 @@ rem netstat -ab
 rem Copy any word to the clipboard
 rem echo krishnaraj.menon|clip
 
-rem Start explorer in the current location of cmd
+rem Start Internet explorer with a URL
 rem start iexplore http://www.google.com
+
+rem Start Windows Explorer with the current location in CMD
+rem start .
 
 rem Launch advanced system properties
 rem SystemPropertiesAdvanced
